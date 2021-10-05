c870c2415bb700f2546bc576950a036b73d331e1
Svarog <alexander.s.zernov@gmail.com>
2021-10-05 16:01:19 +1300

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,193 @@
+"use strict";
+
+// read data from HTML
+const v1xText = document.getElementById("v1_x_text");
+const v1xSlider = document.getElementById("v1_x_slider");
+v1xSlider.oninput = function() {
+  v1xText.value = this.value;
+}
+const v1yText = document.getElementById("v1_y_text");
+const v1ySlider = document.getElementById("v1_y_slider");
+v1ySlider.oninput = function() {
+  v1yText.value = this.value;
+}
+const v1zText = document.getElementById("v1_z_text");
+const v1zSlider = document.getElementById("v1_z_slider");
+v1zSlider.oninput = function() {
+  v1zText.value = this.value;
+}
+
+const v2xText = document.getElementById("v2_x_text");
+const v2xSlider = document.getElementById("v2_x_slider");
+v2xSlider.oninput = function() {
+  v2xText.value = this.value;
+}
+const v2yText = document.getElementById("v2_y_text");
+const v2ySlider = document.getElementById("v2_y_slider");
+v2ySlider.oninput = function() {
+  v2yText.value = this.value;
+}
+const v2zText = document.getElementById("v2_z_text");
+const v2zSlider = document.getElementById("v2_z_slider");
+v2zSlider.oninput = function() {
+  v2zText.value = this.value;
+}
+
+const v3xText = document.getElementById("v3_x_text");
+const v3xSlider = document.getElementById("v3_x_slider");
+v3xSlider.oninput = function() {
+  v3xText.value = this.value;
+}
+const v3yText = document.getElementById("v3_y_text");
+const v3ySlider = document.getElementById("v3_y_slider");
+v3ySlider.oninput = function() {
+  v3yText.value = this.value;
+}
+const v3zText = document.getElementById("v3_z_text");
+const v3zSlider = document.getElementById("v3_z_slider");
+v3zSlider.oninput = function() {
+  v3zText.value = this.value;
+}
+
+const v4xText = document.getElementById("v4_x_text");
+const v4xSlider = document.getElementById("v4_x_slider");
+v4xSlider.oninput = function() {
+  v4xText.value = this.value;
+}
+const v4yText = document.getElementById("v4_y_text");
+const v4ySlider = document.getElementById("v4_y_slider");
+v4ySlider.oninput = function() {
+  v4yText.value = this.value;
+}
+const v4zText = document.getElementById("v4_z_text");
+const v4zSlider = document.getElementById("v4_z_slider");
+v4zSlider.oninput = function() {
+  v4zText.value = this.value;
+}
+
+const v5xText = document.getElementById("v5_x_text");
+const v5xSlider = document.getElementById("v5_x_slider");
+v5xSlider.oninput = function() {
+  v5xText.value = this.value;
+}
+const v5yText = document.getElementById("v5_y_text");
+const v5ySlider = document.getElementById("v5_y_slider");
+v5ySlider.oninput = function() {
+  v5yText.value = this.value;
+}
+const v5zText = document.getElementById("v5_z_text");
+const v5zSlider = document.getElementById("v5_z_slider");
+v5zSlider.oninput = function() {
+  v5zText.value = this.value;
+}
+
+const v6xText = document.getElementById("v6_x_text");
+const v6xSlider = document.getElementById("v6_x_slider");
+v6xSlider.oninput = function() {
+  v6xText.value = this.value;
+}
+const v6yText = document.getElementById("v6_y_text");
+const v6ySlider = document.getElementById("v6_y_slider");
+v6ySlider.oninput = function() {
+  v6yText.value = this.value;
+}
+const v6zText = document.getElementById("v6_z_text");
+const v6zSlider = document.getElementById("v6_z_slider");
+v6zSlider.oninput = function() {
+  v6zText.value = this.value;
+}
+
+const v7xText = document.getElementById("v7_x_text");
+const v7xSlider = document.getElementById("v7_x_slider");
+v7xSlider.oninput = function() {
+  v7xText.value = this.value;
+}
+const v7yText = document.getElementById("v7_y_text");
+const v7ySlider = document.getElementById("v7_y_slider");
+v7ySlider.oninput = function() {
+  v7yText.value = this.value;
+}
+const v7zText = document.getElementById("v7_z_text");
+const v7zSlider = document.getElementById("v7_z_slider");
+v7zSlider.oninput = function() {
+  v7zText.value = this.value;
+}
+
+const v8xText = document.getElementById("v8_x_text");
+const v8xSlider = document.getElementById("v8_x_slider");
+v8xSlider.oninput = function() {
+  v8xText.value = this.value;
+}
+const v8yText = document.getElementById("v8_y_text");
+const v8ySlider = document.getElementById("v8_y_slider");
+v8ySlider.oninput = function() {
+  v8yText.value = this.value;
+}
+const v8zText = document.getElementById("v8_z_text");
+const v8zSlider = document.getElementById("v8_z_slider");
+v8zSlider.oninput = function() {
+  v8zText.value = this.value;
+}
+
+function reset() {
+	v1xText.value = 0;
+	v1xSlider.value = 0;
+	v1yText.value = 0;
+	v1ySlider.value = 0;
+	v1zText.value = 0;
+	v1zSlider.value = 0;
+	
+	v2xText.value = 0;
+	v2xSlider.value = 0;
+	v2yText.value = 0;
+	v2ySlider.value = 0;
+	v2zText.value = 0;
+	v2zSlider.value = 0;
+	
+	v3xText.value = 0;
+	v3xSlider.value = 0;
+	v3yText.value = 0;
+	v3ySlider.value = 0;
+	v3zText.value = 0;
+	v3zSlider.value = 0;
+	
+	v4xText.value = 0;
+	v4xSlider.value = 0;
+	v4yText.value = 0;
+	v4ySlider.value = 0;
+	v4zText.value = 0;
+	v4zSlider.value = 0;
+	
+	v5xText.value = 0;
+	v5xSlider.value = 0;
+	v5yText.value = 0;
+	v5ySlider.value = 0;
+	v5zText.value = 0;
+	v5zSlider.value = 0;
+	
+	v6xText.value = 0;
+	v6xSlider.value = 0;
+	v6yText.value = 0;
+	v6ySlider.value = 0;
+	v6zText.value = 0;
+	v6zSlider.value = 0;
+	
+	v7xText.value = 0;
+	v7xSlider.value = 0;
+	v7yText.value = 0;
+	v7ySlider.value = 0;
+	v7zText.value = 0;
+	v7zSlider.value = 0;
+	
+	v8xText.value = 0;
+	v8xSlider.value = 0;
+	v8yText.value = 0;
+	v8ySlider.value = 0;
+	v8zText.value = 0;
+	v8zSlider.value = 0;
+}
+
+
 // constants
 const EPSILON = 1e-6;
 
@@ -189,26 +379,31 @@ class Voxel {
 		return polygons;
 		
 		function pushPolygonsForEdge(ver1,ver2,ver3,ver4) {
-			const middle13 = ver1.sum(ver3).mult(0.5);
-			const middle24 = ver2.sum(ver4).mult(0.5);
-			const distFromCentre13 = centre.dist(middle13);
-			const distFromCentre24 = centre.dist(middle24);
-			if (distFromCentre13 - distFromCentre24 < EPSILON) {
+			const edge12 = ver2.sub(ver1);
+			const edge13 = ver3.sub(ver1);
+			const edge14 = ver4.sub(ver1);
+			const normal123 = edge12.mult(edge13).normalize();
+			const normal134 = edge13.mult(edge14).normalize();
+			//console.log(1-normal123.dot(normal134));
+			if (1 - normal123.dot(normal134) < EPSILON) {
 				polygons.push(new Polygon(ver1,ver2,ver3,ver4));
 			}
-			else if (distFromCentre13 > distFromCentre24) {
-				polygons.push(new Polygon(ver1,ver2,ver3,ver3));
-				polygons.push(new Polygon(ver1,ver3,ver4,ver4));
-			}
 			else {
-				polygons.push(new Polygon(ver1,ver2,ver4,ver4));
-				polygons.push(new Polygon(ver2,ver3,ver4,ver4));
+				const middle13 = ver1.sum(ver3).mult(0.5);
+				const middle24 = ver2.sum(ver4).mult(0.5);
+				const distFromCentre13 = centre.dist(middle13);
+				const distFromCentre24 = centre.dist(middle24);
+				if (distFromCentre13 >= distFromCentre24) {
+					polygons.push(new Polygon(ver1,ver2,ver3,ver3));
+					polygons.push(new Polygon(ver1,ver3,ver4,ver4));
+				}
+				else {
+					polygons.push(new Polygon(ver1,ver2,ver4,ver4));
+					polygons.push(new Polygon(ver2,ver3,ver4,ver4));
+				}
 			}
 		}
 	}
-	
-
-
 }
 
 class Basis {
@@ -243,13 +438,18 @@ class Basis {
 // global variables
 const mouse = {
 	pos: {x: 0, y: 0},
+	previousPos: {x: 0, y: 0},
 	down: false
 };
 
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
+canvas.width = 600;
+canvas.height = 600;
+
 canvas.addEventListener('mousemove', function (evt) {
+	mouse.previousPos = mouse.pos;
     mouse.pos = getMousePos(canvas, evt);
 }, false);
 
@@ -265,7 +465,7 @@ const isometricBasis = new Basis(new Vector(1,0,0),new Vector(0,1,0),new Vector(
 
 const vox1 = new Voxel(
 	new Vector(-1,-1,-1),// v1
-	new Vector(1.8,-1,-1),// v2
+	new Vector(1,-1,-1),// v2
 	new Vector(1,1,-1),// v3
 	new Vector(-1,1,-1),// v4
 	new Vector(-1,-1,1),// v5
@@ -328,8 +528,9 @@ function step(timestamp) {
 }
 
 function draw(timestamp,deltaTime) {
-	canvas.width = 0.95 * (window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth);
-	canvas.height = 0.95 * (window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight);
+	// canvas.width = 0.95 * (window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth);
+	// canvas.height = 0.95 * (window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight);
+	
 	
 	const quarterWidth = canvas.height / 4;
 	
@@ -337,12 +538,15 @@ function draw(timestamp,deltaTime) {
 	
 	testDraw(ctx);
 	
+	updateVoxel(vox1);
+	rotateBasis(isometricBasis);
+	
 	// function drawVoxel(ctx, voxel, shift = new Vector(0,0,0), view = 'front', scale = 64)
 	
-	drawVoxel(ctx, vox1, new Vector(quarterWidth,quarterWidth,0), 'front', quarterWidth / 2);
-	drawVoxel(ctx, vox1, new Vector(3*quarterWidth,quarterWidth,0), 'right', quarterWidth / 2);
-	drawVoxel(ctx, vox1, new Vector(quarterWidth,3*quarterWidth,0), 'top', quarterWidth / 2);
-	drawVoxel(ctx, vox1, new Vector(3*quarterWidth,3*quarterWidth,0), 'isometric', quarterWidth / 2);
+	drawVoxel(ctx, vox1, new Vector(quarterWidth,quarterWidth,0), 'front', quarterWidth / 4);
+	drawVoxel(ctx, vox1, new Vector(3*quarterWidth,quarterWidth,0), 'right', quarterWidth / 4);
+	drawVoxel(ctx, vox1, new Vector(quarterWidth,3*quarterWidth,0), 'top', quarterWidth / 4);
+	drawVoxel(ctx, vox1, new Vector(3*quarterWidth,3*quarterWidth,0), 'isometric', quarterWidth / 4);
 }
 
 function drawVoxel(ctx, voxel, shift = new Vector(0,0,0), view = 'front', scale = 64) {
@@ -415,4 +619,45 @@ function drawVoxel(ctx, voxel, shift = new Vector(0,0,0), view = 'front', scale 
 		ctx.fill();
 	}
 	
+}
+
+function updateVoxel(voxel) {
+	voxel.v1.x = parseInt(v1xSlider.value)/8 - 1;
+	voxel.v1.y = parseInt(v1ySlider.value)/8 - 1;
+	voxel.v1.z = parseInt(v1zSlider.value)/8 - 1;
+
+	voxel.v2.x = parseInt(v2xSlider.value)/8 + 1;
+	voxel.v2.y = parseInt(v2ySlider.value)/8 - 1;
+	voxel.v2.z = parseInt(v2zSlider.value)/8 - 1;
+
+	voxel.v3.x = parseInt(v3xSlider.value)/8 + 1;
+	voxel.v3.y = parseInt(v3ySlider.value)/8 + 1;
+	voxel.v3.z = parseInt(v3zSlider.value)/8 - 1;
+
+	voxel.v4.x = parseInt(v4xSlider.value)/8 - 1;
+	voxel.v4.y = parseInt(v4ySlider.value)/8 + 1;
+	voxel.v4.z = parseInt(v4zSlider.value)/8 - 1;
+
+	voxel.v5.x = parseInt(v5xSlider.value)/8 - 1;
+	voxel.v5.y = parseInt(v5ySlider.value)/8 - 1;
+	voxel.v5.z = parseInt(v5zSlider.value)/8 + 1;
+
+	voxel.v6.x = parseInt(v6xSlider.value)/8 + 1;
+	voxel.v6.y = parseInt(v6ySlider.value)/8 - 1;
+	voxel.v6.z = parseInt(v6zSlider.value)/8 + 1;
+
+	voxel.v7.x = parseInt(v7xSlider.value)/8 + 1;
+	voxel.v7.y = parseInt(v7ySlider.value)/8 + 1;
+	voxel.v7.z = parseInt(v7zSlider.value)/8 + 1;
+
+	voxel.v8.x = parseInt(v8xSlider.value)/8 - 1;
+	voxel.v8.y = parseInt(v8ySlider.value)/8 + 1;
+	voxel.v8.z = parseInt(v8zSlider.value)/8 + 1;
+}
+
+function rotateBasis(basis) {
+	if (mouse.down) {
+		basis.rotate((mouse.pos.x - mouse.previousPos.x)/100, new Vector(0,1,0));
+		basis.rotate((mouse.pos.y - mouse.previousPos.y)/100, new Vector(-1,0,0));
+	}
 }
